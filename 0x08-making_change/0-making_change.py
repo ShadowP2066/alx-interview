d01f8b44ae4095d556a846164cb35cff97cbadb5
Shadow P <117807310+ShadowP2066@users.noreply.github.com>
2023-11-23 09:27:40 +0000

    Updated commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-"""
-Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
-"""
+""" Making changes """
+
 
 def makeChange(coins, total):
+    """ Generate changes needed to reach total
+
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
+    """
     if total <= 0:
         return 0
-
-    # Initialize an array to store the minimum number of coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    # Update the dp array based on the available coins
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # If dp[total] is still infinite, the total cannot be met by any combination of coins
-    return dp[total] if dp[total] != float('inf') else -1
-
-# Test cases
-print(makeChange([1, 2, 25], 37))  # Output: 7
-print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output: -1
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
